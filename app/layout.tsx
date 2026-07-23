86300777b9d821080f85dea899f654ce2d71f881
twanty979-hue <twanty979@gmail.com>
2026-07-23 13:31:59 +0700

    feat(analytics): integrate Meta Pixel tracking code in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -118,6 +118,32 @@ export default async function RootLayout({
             </Script>
           </>
         )}
+
+        {/* Meta Pixel (Facebook Pixel) */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1575029190933851');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1575029190933851&ev=PageView&noscript=1"
+            alt="facebook-pixel"
+          />
+        </noscript>
+
         {/* JSON-LD Schema for Organization + WebSite */}
         <script
           type="application/ld+json"
